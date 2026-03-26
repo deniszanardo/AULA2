53dd2c7002e732042d7ca1d033b5ee5a88f5c4fe
deniszanardo <deniszanardo@gmail.com>
2026-03-25 21:11:48 -0300

    Add files via upload

---
+++ AULA 2/AULA 2 COMPRA COM DESCONTO.py
@@ -0,0 +1,18 @@
+#Peça o valor de um produto e:
+produto = float(input("Digite o preço do produto "))
+
+#Calcule um desconto de 10%.
+preco = (produto *0.10)
+#        produto 100 - 10 % = 90  
+#Mostre o valor final.
+print(preco)
+
+#Verifique se o valor final é maior que 100.
+print(preco > 100)
+
+#Verifique se o produto ficou barato (menor que 50).
+print(preco < 50)
+
+
+
+
